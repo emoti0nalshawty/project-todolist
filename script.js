48a954d64f9271d44ffb435fafb93b2f97b0510e
emoti0nalshawty <san.avvakum@gmail.com>
2020-12-16 15:20:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const submitForm = document.querySelector('.add');
+const addButton = document.querySelector('.add-todo');
+const todoList = document.querySelector('.todos');
+const list = document.querySelectorAll('.todos li');
+
+let listlength = list.length;
+
+const generateTemplate = todo => {
+    const html = `<li>
+                    <input type="checkbox" id="todo_${listlength}">
+                    <label for="todo_${listlength}">
+                        <span class="check"></span>
+                        ${todo}
+                    </label>
+                    <i class="far fa-trash-alt delete"></i>
+                  </li>`
+    todoList.innerHTML += html
+};
+
+function addTodos(e){
+    e.preventDefault();
+    const todo = submitForm.add.value.trim();
+    if (todo.length) {
+        listlength = listlength + 1;
+        generateTemplate(todo)
+        submitForm.reset()
+    }
+}
+
+submitForm.addEventListener('submit', addTodos)
+addButton.addEventListener('click', addTodos)
+
+function deleteTodos(e) {
+    if (e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+}
+
+todoList.addEventListener('click', deleteTodos)
